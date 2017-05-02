147831e4bbf7d4aa5f8178d68b5c4a8b8cd77f6a
Cyrus Harmon <cyrus@bobobeach.com>
2017-05-01 23:00:22 -0700

    add bezier-app-2

--- bezier-app.lisp
+++ bezier-app.lisp
@@ -30,3 +30,36 @@
 (defun bezier-app-main ()
   (run-frame-top-level (make-application-frame 'bezier-app)))
 
+
+(define-application-frame bezier-app-2 ()
+  ()
+  (:panes
+   (app :application :height 400 :width 600
+        :display-function 'display-bezier-2)
+   (int :interactor :height 200 :width 600))
+  (:layouts
+   (default (vertically () app int))))
+
+(defun display-bezier-2 (frame pane)
+  (declare (ignore frame))
+  (let ((line-thickness 4)
+        (design
+         (climi::make-bezier-thing*
+          'climi::bezier-area
+          (list 34 44 -100 128 347 124 247 256))))
+    (formatting-table (pane :x-spacing 20
+                                :y-spacing 20)
+      (formatting-row (pane)
+        (formatting-cell (pane :align-x :center
+                               :align-y :center)
+          (draw-rectangle* pane 34 44 300 300 :ink +green+ :filled t))
+        (formatting-cell (pane :align-x :center
+                               :align-y :center)
+          (draw-rectangle* pane 34 44 300 300 :ink +yellow+ :filled t)
+          (climi::draw-bezier-design* pane design
+                                      :line-thickness line-thickness
+                                      :ink +blue+))))))
+
+(defun bezier-app-2-main ())
+(run-frame-top-level (make-application-frame 'bezier-app-2))
+

a5c86c01c46c0aabd53fccf5a3e02b5a67698304
Cyrus Harmon <cyrus@bobobeach.com>
2017-05-01 21:22:20 -0700

    first pass at McCLIM Bezier curve drawing tests

---
+++ bezier-test.lisp
@@ -0,0 +1,112 @@
+
+(defpackage #:bezier-test
+  (:use #:clim #:clim-lisp #:clim-extensions))
+
+(in-package #:bezier-test)
+
+(defun bezier-test-1 (&key (file #p"bezier-figure-1.ps"))
+  (format t ";; Creating ~S.~%" file)
+  (with-open-file (file-stream file :direction :output :if-exists :supersede)
+    (with-output-to-postscript-stream
+        (stream file-stream
+                :header-comments '(:title "Foo")
+                :scale-to-fit t)
+        (formatting-table (stream :x-spacing 20
+                                  :y-spacing 20)
+          (formatting-row (stream)
+            (formatting-cell (stream :align-x :left
+                                     :align-y :center)
+              (let ((line-thickness 4))
+                (draw-circle* stream 30 30 20
+                              :start-angle (/ pi 2)
+                              :end-angle (+ (/ pi 2) pi)
+                              :filled nil
+                              :line-thickness line-thickness)
+                (draw-circle* stream 60 30 20
+                              :start-angle (+ (/ pi 2) pi)
+                              :end-angle (/ pi 2)
+                              :filled nil
+                              :line-thickness line-thickness)
+                (let ((design
+                       (climi::make-bezier-thing*
+                        'climi::bezier-area
+                        (list 34 44 34 128 147 44 247 256))))
+                  (draw-rectangle* stream 0 0 10 10 :ink +green+)
+                  (draw-rectangle* stream 3 3 13 13 :ink +red+)
+                  (draw-rectangle* stream 34 44 247 256 :ink +yellow+)
+                  (climi::draw-bezier-design* stream design
+                                              :line-thickness line-thickness
+                                              :ink +sea-green+))))))))
+  file)
+
+(uiop:run-program `("ps2pdf" ,(uiop:unix-namestring (bezier-test-1))))
+
+(defun bezier-test-2 (&key (file #p"bezier-figure-2.ps"))
+  (format t ";; Creating ~S.~%" file)
+  (with-open-file (file-stream file :direction :output :if-exists :supersede)
+    (with-output-to-postscript-stream
+        (stream file-stream
+                :header-comments '(:title "Foo")
+                :scale-to-fit t)
+        (formatting-table (stream :x-spacing 20
+                                  :y-spacing 20)
+
+          (formatting-row (stream)
+            (formatting-cell (stream :align-x :center
+                                     :align-y :bottom
+                                     :min-height 110)
+              (draw-text* stream "Bezier Test" 170 30
+                          :text-style (make-text-style :fix :bold :normal))))
+          (formatting-row (stream)
+            (formatting-cell (stream :align-x :left
+                                     :align-y :center)
+              (let ((line-thickness 4))
+                (draw-circle* stream 30 30 20
+                              :start-angle (/ pi 2)
+                              :end-angle (+ (/ pi 2) pi)
+                              :filled nil
+                              :line-thickness line-thickness)
+
+                (draw-circle* stream 60 30 20
+                              :start-angle (+ (/ pi 2) pi)
+                              :end-angle (/ pi 2)
+                              :filled nil
+                              :line-thickness line-thickness)
+                (let ((design
+                       (climi::make-bezier-thing*
+                        'climi::bezier-area
+                        (list 34 44 34 128 147 44 247 256))))
+                  (draw-rectangle* stream 0 0 10 10 :ink +green+)
+                  (draw-rectangle* stream 3 3 13 13 :ink +red+)
+                  (draw-rectangle* stream 34 44 247 256 :ink +yellow+)
+                  (climi::draw-bezier-design* stream design
+                                              :line-thickness line-thickness
+                                              :ink +black+))))
+            (formatting-cell (stream :align-x :left
+                                     :align-y :center)
+              (let ((line-thickness 4))
+                (draw-circle* stream 30 30 20
+                              :start-angle (/ pi 2)
+                              :end-angle (+ (/ pi 2) pi)
+                              :filled nil
+                              :line-thickness line-thickness)
+
+                (draw-circle* stream 60 30 20
+                              :start-angle (+ (/ pi 2) pi)
+                              :end-angle (/ pi 2)
+                              :filled nil
+                              :line-thickness line-thickness)
+                (let ((design
+                       (climi::make-bezier-thing*
+                        'climi::bezier-area
+                        (list 34 44 34 128 147 44 247 256))))
+                  (draw-rectangle* stream 0 0 10 10 :ink +green+)
+                  (draw-rectangle* stream 3 3 13 13 :ink +red+)
+                  (draw-rectangle* stream 34 44 247 256 :ink +pink+)
+                  (climi::draw-bezier-design* stream design
+                                              :line-thickness line-thickness
+                                              :ink +sea-green+))))))))
+  file)
+
+(uiop:run-program `("ps2pdf" ,(uiop:unix-namestring (bezier-test-2))))
+
